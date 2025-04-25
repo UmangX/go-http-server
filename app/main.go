d7aaf83d6d6ecd44b124ad30ee32136394edc8e9
Umang Patel <69800542+UmangX@users.noreply.github.com>
2025-04-25 20:17:14 +0530

    completed the first challenge

--- app/main.go
+++ app/main.go
@@ -12,18 +12,22 @@ var _ = os.Exit
 
 func main() {
 	fmt.Println("Logs from your program will appear here!")
-
-	//Uncomment this block to pass the first stage
-
-	l, err := net.Listen("tcp", "0.0.0.0:4221")
-	if err != nil {
-		fmt.Println("Failed to bind to port 4221")
-		os.Exit(1)
+	line, _ := net.Listen("tcp", ":4221")
+	for {
+		conn, _ := line.Accept()
+		go HandleConn(conn)
 	}
+}
 
-	_, err = l.Accept()
-	if err != nil {
-		fmt.Println("Error accepting connection: ", err.Error())
-		os.Exit(1)
-	}
+func HandleConn(conn net.Conn) {
+	defer conn.Close()
+	buffer := make([]byte, 2048)
+	conn.Read(buffer)
+	fmt.Println(string(buffer))
+	response := "HTTP/1.1 200 OK\r\n" +
+		"Content-Type: text/plain\r\n" +
+		"Content-Length: 13\r\n" +
+		"\r\n" +
+		"Hello, world!"
+	conn.Write([]byte(response))
 }
