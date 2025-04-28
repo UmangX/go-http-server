73498363190088ee809d4c7e745c7161d46c62f8
Umang Patel <69800542+UmangX@users.noreply.github.com>
2025-04-28 20:23:51 +0530

    added the user-agent endpoint

--- Makefile
+++ Makefile
@@ -1,2 +1,7 @@
-local:
+run:
 	go run ./app 
+empty:
+	curl -v http://localhost:4221/
+test:
+	curl -v --header "User-Agent: foobar/1.2.3" http://localhost:4221/user-agent
+	
--- app/main.go
+++ app/main.go
@@ -53,17 +53,19 @@ func HandleConn(conn net.Conn) {
 	conn.Read(buffer)
 
 	content := string(buffer)
+
+	// this line is the main content seperate through new line
 	lines := strings.Split(content, "\n")
 
 	// when this strings is done will work on using the direct bytes for this
 	//req_type := strings.Split(lines[0], " ")[0]
 	url_text := strings.Split(lines[0], " ")[1]
 
-	lines = strings.Split(url_text, "/")
+	url_lines := strings.Split(url_text, "/")
 	if lines[1] == "echo" {
 		//fmt.Printf("this is the echo link with /%v \n", lines[2])
-		follow_up := lines[2]
-		urlstr := lines[2][0:]
+		follow_up := url_lines[2]
+		urlstr := url_lines[2][0:]
 		if follow_up == "/" {
 			conn.Write([]byte(generate_content_respone(" ")))
 			return
@@ -71,6 +73,13 @@ func HandleConn(conn net.Conn) {
 			conn.Write([]byte(generate_content_respone(urlstr)))
 			return
 		}
+	}
+
+	// write for the header actions
+	if url_lines[1] == "user-agent" {
+		user_agent := strings.Split(lines[3], ":")[1]
+		fmt.Println(user_agent)
+		conn.Write([]byte(generate_content_respone(user_agent)))
 	}
 
 	//fmt.Printf("req type %v  url_text %v len for the lines in url_text : %v  this is the first in lines : %v \n", req_type, url_text, len(lines), lines[0])
